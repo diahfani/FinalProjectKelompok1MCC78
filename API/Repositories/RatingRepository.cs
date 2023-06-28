0193c6b6490f19a199a3a586fb956c934d4e9f89
Tama <yoggiepratama97@gmail.com>
2023-06-28 13:39:22 +0700

    Add GetRatingByEmployeeId, GetRatingByTaskId

--- API/Repositories/RatingRepository.cs
+++ API/Repositories/RatingRepository.cs
@@ -1,22 +1,58 @@
 ﻿using API.Contracts;
 using API.Model;
 using API.ViewModel.Employee;
+using API.ViewModel.Rating;
 
 namespace API.Repositories;
 
 public class RatingRepository : GeneralRepository<Rating>, IRatingRepository
 {
-    public RatingRepository(ProjectManagementDBContext context) : base(context)
+    private readonly IEmployeeRepository _employeeRepository;
+    public RatingRepository(ProjectManagementDBContext context,     
+                            IEmployeeRepository employeeRepository) : base(context)
     {
+        _employeeRepository = employeeRepository;
     }
 
-    public Rating GetRatingByEmployeeId(Guid employeeId)
+    public IEnumerable<RatingVM> GetRatingByEmployeeId(Guid employeeId)
     {
-        return _context.Set<Rating>().Find(employeeId);
+        var ratings = _context.Set<Rating>().Where(r => r.Guid == employeeId).ToList();
+        var ratingVM = new List<RatingVM>();
+
+        foreach (var rating in ratings)
+        {
+            var data = new RatingVM
+            {
+                Guid = rating.Guid,
+                RatingValue = rating.RatingValue,
+                Comment = rating.Comment,
+                CreatedDate = rating.CreatedDate,
+                ModifiedDate = rating.ModifiedDate
+            };
+            ratingVM.Add(data);
+        }
+        return ratingVM;
     }
 
-    public Rating GetRatingByTaskId(Guid taskId)
+    public IEnumerable<RatingVM> GetRatingByTaskId(Guid taskId)
     {
-        return _context.Set<Rating>().Find(taskId);
+        var tasks = _context.Set<Model.Task>().Where(t => t.Guid == taskId);
+        var ratings = _context.Set<Rating>().Where(r => r.Guid == taskId).ToList();
+        var ratingsVM = new List<RatingVM>();
+
+        tasks ??= null;
+        foreach (var rating in ratings)
+        {
+            var data = new RatingVM
+            {
+                Guid = rating.Guid,
+                RatingValue = rating.RatingValue,
+                Comment = rating.Comment,
+                CreatedDate = rating.CreatedDate,
+                ModifiedDate = rating.ModifiedDate,
+            };
+            ratingsVM.Add(data);
+        }
+        return ratingsVM;
     }
 }
---
+++ Client/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Client.Repositories.Interface;
+using Client.ViewModels;
+using System.Security.Claims;
+
+namespace Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAccountRepository repository;
+
+        public AccountController(IAccountRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Logins()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logins(LoginVM login)
+        {
+            var result = await repository.Logins(login);
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            else if (result.Code == 409)
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                return View();
+            }
+            if (result.Code == 200)
+            {
+                HttpContext.Session.SetString("JWToken", result.Data);
+                
+                if (User.HasClaim(ClaimTypes.Role,"manager"))
+                {
+                    return RedirectToAction("Manager", "Home");
+                } 
+                else if(User.HasClaim(ClaimTypes.Role, "manager"))
+                {
+                    return RedirectToAction("Employee", "Home");
+                }
+            }
+            return View();
+
+        }
+
+        [HttpGet("/Logout")]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("/Account/Logins");
+        }
+
+        [HttpGet]
+        public IActionResult Registers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        /*[ValidateAntiForgeryToken]*/
+        public async Task<IActionResult> Registers(RegisterVM registerVM)
+        {
+
+            var result = await repository.Registers(registerVM);
+            if (result is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            else if (result.StatusCode == "409")
+            {
+                ModelState.AddModelError(string.Empty, result.Message);
+                TempData["Error"] = $"Something Went Wrong! - {result.Message}!";
+                return View();
+            }
+            else if (result.StatusCode == "200")
+            {
+                TempData["Success"] = $"Data has been Successfully Registered! - {result.Message}!";
+                return RedirectToAction("Login", "Account");
+            }
+            return RedirectToAction("Index", "Employee");
+        }
+    }
+}
