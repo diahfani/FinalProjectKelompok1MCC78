919be782548a138a7df034f02454a8afd1321a57
Diah Aufa Arini <diahaufaarini5@gmail.com>
2023-06-30 21:58:47 +0700

    crud task DONE!

--- Client/ViewModels/ResponseMessageVM.cs
+++ Client/ViewModels/ResponseMessageVM.cs
@@ -2,6 +2,9 @@
 {
     public class ResponseMessageVM
     {
+        /*public int Code { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }*/
         public int StatusCode { get; set; }
         public string Message { get; set; }
     }
